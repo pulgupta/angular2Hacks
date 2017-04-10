ef128b65e7e79150bf0da81c5bec6d50f8fa137a
Pulkit Gupta <pulgupta@redhat.com>
2017-04-10 17:15:28 +0530

    updated the code

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component } from   '@angular/core';
 
 //Custom class
 export class Hero {
@@ -6,26 +6,103 @@ export class Hero {
   name: string;
 }
 
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   selector: 'my-app',  //<----Name of the html element this component will render
-  
+
   //<----View to show this data------>
-  template: `  
+  template: `
     <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+    <!--Iterating through the list of heroes-->
+      <li *ngFor="let hero of heroes">
+      <!--For each of the hero we are displaying the data-->
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+
     <h2>{{hero.name}} details!</h2>
     <div><label>id: </label>{{hero.id}}</div>
     <div><label>name: </label>{{hero.name}}</div>
     <div>
-    <label>name: </label>
-    <input [(ngModel)]="hero.name" placeholder="name">
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
     </div>`,
+
+    styles: [`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .heroes li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .heroes li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
+
+
 })
 
 //<----Exporting the class so that it is available outside the file---->
-export class AppComponent  { 
+export class AppComponent  {
     title = 'Tour of Heroes';
     hero: Hero = {                   //<----Initialising the hero class
                     id: 1,
                     name: 'Windstorm_class'
                 };
+    //The data type of heroes will be derived from HEROES which is a feature of typescript
+    //We are addding this here to expose it outside
+    heroes=HEROES
 }
